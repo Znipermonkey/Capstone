19e8098c38b75cefe3b404677db548686b8d7e7e
Robert Free <144733501+Znipermonkey@users.noreply.github.com>
2023-11-02 11:56:20 -0600

    capstone

---
+++ server/controller.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const {CONNECTION_STRING} = process.env
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize(CONNECTION_STRING)
+
+app.use(express.json())
+app.use(cors())
+
+module.exports = {
+    save: (req,res) => {
+        sequelize.query(`Insert into Saved (number),
+        values(${calculator.displayValue})
+        `).then(dbRes => res.status(200).send(dbRes[0]))
+    },
+
+    post: (req,res) => {
+        sequelize.query(`
+        SELECT * FROM Saved,
+        `).then(dbRes => res.status(200).send(dbRes[0]))
+    }
+}
